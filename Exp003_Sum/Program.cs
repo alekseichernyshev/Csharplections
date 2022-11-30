21e2c2707f8adf214ed91930d3e568dc824e2d1c
alekseichernyshev <adanayrel@mail.ru>
2022-11-30 23:05:05 +0900

    Add Random

--- Exp003_Sum/Program.cs
+++ Exp003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int numberB = 51;
+﻿int numberA = new Random().Next(1,10); // случайное число от 1 до 9 включительно
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
