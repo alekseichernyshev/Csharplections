6f839c00749b747dfcd550cad1dddce9ca861d51
alekseichernyshev <adanayrel@mail.ru>
2022-12-02 20:32:10 +0900

    Поиск индекса

---
+++ Exp010_Method/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 13, 21, 13, 41, 91, 51, 16, 17, 81, 91 };
+
+int n = array.Length;
+int find = 91;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
